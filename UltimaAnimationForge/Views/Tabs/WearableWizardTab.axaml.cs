5f229182307e6d0bd662fb34236a254a59f6a2a3
Dragon Slayer <85514184+DragonSlayer62@users.noreply.github.com>
2026-06-06 20:01:25 -0500

    Create WearableWizardTab.axaml.cs

---
+++ UltimaAnimationForge/Views/Tabs/WearableWizardTab.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace UltimaAnimationForge.Views.Tabs;
+
+public partial class WearableWizardTab : UserControl
+{
+    public WearableWizardTab()
+    {
+        InitializeComponent();
+    }
+}
